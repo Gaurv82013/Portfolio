9da3db90bf711eeeec80a0029436945e5de95503
Gaurav Kumar <gauravkumar803109@gmail.com>
2026-07-11 12:30:59 +0530

    Added widgiq chatbot sdk

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,23 @@ import { Home } from "@/pages/Home"
 import { NotFound } from "@/pages/NotFound"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
 
 
 function App() {
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://widgiq.com/widgiq-widget.iife.js";
+    script.setAttribute("data-widgiq-widget", "");
+    script.setAttribute("data-api-key", import.meta.env.VITE_WIDGIQ_API_KEY);
+    script.defer = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   return (
     <>
